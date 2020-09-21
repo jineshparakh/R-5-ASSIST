8331f5e9eeee7e04fb96984e2b233d6490a9c62e
jineshparakh <jineshparakh@hotmail.com>
2020-09-21 14:07:39 +0530

    Added giving tests

---
+++ flaskbackend.py
@@ -0,0 +1,188 @@
+from flask import Flask, Response,redirect, url_for, request
+import flask
+# import summarize2.views as sv
+import _sqlite3
+from display_questions import StoreQuestions
+import os
+from flask import Flask,render_template,url_for,request,jsonify,make_response,redirect
+from flask_mysqldb import MySQL
+import form
+from werkzeug.exceptions import BadRequestKeyError
+import yaml
+from hello import app2 as app2
+from form import LoginForm
+app = Flask(__name__, template_folder="templates",static_folder="static")
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+# {% comment %} <form class="form-inline" action="http://localhost:5000/q={{ q_id }}" method="post"> {% endcomment %}
+db=yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+
+mysql=MySQL(app)
+name =""
+acc_holder = ""
+test_dict = {}
+
+####################################Helper functions################################################
+
+def check_answer(data):
+    global test_dict
+    store = StoreQuestions() 
+    print(test_dict)
+    print("-------------------")
+    ans_dict = {"A":1,"B":2,"C":3,"D":4}
+    for key in  test_dict.keys(): 
+        if(test_dict.get(key)==ans_dict.get(data.iloc[key,6])):
+            store.store_results(True,key)
+        else:
+            store.store_results(False,key)
+
+
+
+
+
+
+
+
+
+
+@app.route('/q=<int:q_id>',methods = ['POST', 'GET'])
+def test_page2(q_id):
+   store = StoreQuestions() 
+   data = store.get_questions(31)
+   ans_dict = {"A":1,"B":2,"C":3,"D":4}
+   global test_dict
+   q_id = q_id-1
+   if request.method == 'POST':
+        try:
+            user = request.form["SAE"]
+            test_dict[q_id] = ans_dict.get(data.iloc[q_id,6])
+            q_id=q_id+2
+            return redirect(url_for('test_page2',q_id = q_id))
+        except BadRequestKeyError as bk:
+            print("oops")
+        try: 
+            temp = request.form['back-button']
+            # q_id=q_id-1
+            return redirect(url_for('test_page2',q_id = q_id))
+        except BadRequestKeyError as bk:
+            print("oops2")
+        try: 
+            temp = request.form['submit-button']
+            check_answer(data
+            )
+            return  redirect(url_for('submission'))
+        except BadRequestKeyError as bk:
+            print("oops2")
+
+   return flask.render_template('give_test.html', q_id = data['q_id'].unique()[q_id],
+                                question = data['question'].unique()[q_id],
+                                option_A = data['option_A'].unique()[q_id],
+                                option_B = data['option_B'].unique()[q_id],
+                                option_C = data['option_C'].unique()[q_id],
+                                option_D = data['option_D'].unique()[q_id],)
+
+
+
+
+
+def allowed_pdf(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config["ALLOWED_EXTENSIONS"]:
+        return True
+    return False
+
+@app.route('/upload-pdf', methods=["GET", "POST"])
+def upload_pdf():
+    if request.method == "POST":
+        if request.files:
+            pdf = request.files["pdf"]
+            mail = request.form['email']
+
+            if pdf.filename == "":
+                return render_template('public/upload_pdf.html')
+            if not allowed_pdf(pdf.filename):
+                return render_template('public/upload_pdf.html')
+            else:
+                filename = 'pdf_file.pdf'
+                pdf.save(os.path.join(app.config["PDF_UPLOADS"], filename))
+                thread = Thread(target = pdfParser, kwargs={'filename': os.path.join(app.config["PDF_UPLOADS"], 'pdf_file.pdf'), 'mailid': f'{mail}'})
+                thread.start()
+                return render_template('public/upload_pdf.html')
+        return redirect(request.url)
+    return render_template('public/upload_pdf.html')
+
+
+
+@app.route("/dashboard_teacher")
+def dash_teacher():
+    return render_template('dashboard.html')
+
+
+@app.route("/dashboard_student")
+def dash_stu():
+    return render_template('dashboard_students.html')
+
+
+@app.route('/submitted',methods = ['POST', 'GET'])
+def submission():
+    if request.method == 'POST':
+        return redirect(url_for('hello_world'))
+    global name
+    return flask.render_template('success.html',name = name)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#@app.route('/',methods=['post'])
+#def login_cred():
+
+#    return render_template('index.html')
+
+@app.route('/login',methods=["POST"])
+def create_entry():
+    res=request.get_json()
+    if res == None:
+        usr=request.form['loginemail']
+        passwd=request.form['loginPassword']
+        cur=mysql.connection.cursor()
+        cur.execute("SELECT * FROM user")
+        record=cur.fetchall()
+        for row in record:
+            if usr in row[2] and passwd in row[3]:
+                print("here")
+                return redirect(url_for('first'))
+        mysql.connection.commit()
+        cur.close()
+        print(usr)
+        return redirect(url_for('sorry'))
+
+    else :
+        print(res)
+        print(res['emailInput_d'])
+        cur=mysql.connection.cursor()
+        global name, acc_holder
+        name = res['nameInput_d']
+        acc_holder = res['selectInput_d']
+        cur.execute("INSERT INTO user(acctype,name,email,password) VALUES(%s,%s,%s,%s)",(res['selectInput_d'],res['nameInput_d'],res['emailInput_d'],res['passwordInput_d']))
+        mysql.connection.commit()
+        cur.close()
+    return redirect(url_for('first'))
+
+
+@app.route('/success')
+def first():
+    return redirect(url_for('my_dash_app')) 
+
+@app.route('/sorry')
+def sorry():
+    return redirect(url_for('login')) 
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ static/gt_script.js
@@ -0,0 +1,204 @@
+
+$('.oa').click(function() {
+    $('.sss').prop('value', "a");
+  })
+
+  $('.ob').click(function() {
+   
+    $('.sss').prop('value', "b");
+  })
+
+
+  $('.oc').click(function() {
+    $('.sss').prop('value', "c");
+  })
+
+
+$('.od').click(function() {
+    $('.sss').prop('value', "d");
+  })
+
+
+
+$('.op1').click(function() {
+    $(this).find('.left').find('.c2').click();
+});
+
+
+$('.c2').click(function() {
+    if ($(this).hasClass('clicked')) {
+        $(this).removeClass('clicked');
+        $(this).find('p').removeClass('hide');
+        $(this).parent().parent().parent().removeClass('glow');
+    } else {
+        $(this).addClass('clicked');
+        $(this).parent().parent().parent().addClass('glow');
+        $(this).find('p').addClass('hide');
+        if ($(this).attr("id") != "a") {
+            $("#a").removeClass('clicked');
+            $("#a > p").removeClass('hide');
+            $("#a").parent().parent().parent().removeClass('glow');
+        }
+        if ($(this).attr("id") != 'b') {
+            $('#b').removeClass('clicked');
+            $("#b > p").removeClass("hide");
+            $("#b").parent().parent().parent().removeClass('glow');
+        }
+        if ($(this).attr("id") != 'c') {
+            $('#c').removeClass('clicked');
+            $("#c > p").removeClass("hide");
+            $("#c").parent().parent().parent().removeClass('glow');
+        }
+        if ($(this).attr("id") != 'd') {
+            $('#d').removeClass('clicked');
+            $("#d > p").removeClass("hide");
+            $("#d").parent().parent().parent().removeClass('glow');
+        }
+    }
+
+});
+
+
+$(function() {
+    $('on-hover').hover(function() {
+        $('on-hover').attr('i', 'fa fa-bookmark');
+    })
+});
+var Dict={};
+function saveQuestion() {
+  console.log("LOLLLLLLL");
+  var curUrl=window.location.href;
+  var x=curUrl.split("%3D");
+  var qNo=1;
+  if(parseInt(x[1])){
+    qNo=parseInt(s[1]);
+  }
+  var answer;
+  if($('oa').hasClass('clicked')){
+    answer="A";
+  }
+  else if ($('ob').hasClass('clicked')){
+    answer="B";
+  }
+  else if(($('oc').hasClass('clicked'))){
+    answer="C";
+  }
+  else{
+    answer="D";
+  }
+  dict[String(qNo)]=answer;
+  for(key in dict){
+    var val=dict[key];
+    console.log(String(key)+"  "+String(val));
+  }
+
+}
+
+
+
+
+
+// // The JS for the timer
+
+
+// $(document).ready(function() {
+//     TweenLite.defaultEase = Expo.easeOut;
+
+//     initTimer("00:00"); // other ways --> "0:15" "03:5" "5:2"
+
+//     //var reloadBtn = document.querySelector('.reload');
+//     var timerEl = document.querySelector('.timer');
+//     var timer = sessionStorage.getItem('timer_station');
+// if (!timer){
+//     timer =   initTimer("00:00")
+// }
+//     function initTimer(t) {
+
+//         var self = this,
+//             timerEl = document.querySelector('.timer'),
+//             minutesGroupEl = timerEl.querySelector('.minutes-group'),
+//             secondsGroupEl = timerEl.querySelector('.seconds-group'),
+
+//             minutesGroup = {
+//                 firstNum: minutesGroupEl.querySelector('.first'),
+//                 secondNum: minutesGroupEl.querySelector('.second')
+//             },
+
+//             secondsGroup = {
+//                 firstNum: secondsGroupEl.querySelector('.first'),
+//                 secondNum: secondsGroupEl.querySelector('.second')
+//             };
+
+//         var time = {
+//             min: t.split(':')[0],
+//             sec: t.split(':')[1]
+//         };
+
+//         var timeNumbers;
+
+//         function updateTimer() {
+
+//             var timestr;
+//             var date = new Date();
+
+//             date.setHours(0);
+//             date.setMinutes(time.min);
+//             date.setSeconds(time.sec);
+
+//             var newDate = new Date(date.valueOf() - 1000);
+//             var temp = newDate.toTimeString().split(" ");
+//             var tempsplit = temp[0].split(':');
+
+//             time.min = tempsplit[1];
+//             time.sec = tempsplit[2];
+
+//             timestr = time.min + time.sec;
+//             timeNumbers = timestr.split('');
+//             updateTimerDisplay(timeNumbers);
+
+
+//             if (timestr === '1000') {
+//                 $('.num').addClass("red");
+//                 $('.clock-separator').addClass("red");
+//             }
+
+//             if (timestr === '0000')
+//                 countdownFinished();
+
+//             if (timestr != '0000')
+//                 setTimeout(updateTimer, 1000);
+
+//         }
+
+//         function updateTimerDisplay(arr) {
+
+//             animateNum(minutesGroup.firstNum, arr[0]);
+//             animateNum(minutesGroup.secondNum, arr[1]);
+//             animateNum(secondsGroup.firstNum, arr[2]);
+//             animateNum(secondsGroup.secondNum, arr[3]);
+
+//         }
+
+//         function animateNum(group, arrayValue) {
+
+//             TweenMax.killTweensOf(group.querySelector('.number-grp-wrp'));
+//             TweenMax.to(group.querySelector('.number-grp-wrp'), 1, {
+//                 y: -group.querySelector('.num-' + arrayValue).offsetTop
+//             });
+
+//         }
+
+//         setTimeout(updateTimer, 1000);
+
+//     }
+
+//     function countdownFinished() {
+//         setTimeout(function() {
+//             //TweenMax.set(reloadBtn, { scale: 0.8, display: 'block' });
+//             TweenMax.to(timerEl, 1, { opacity: 0 });
+//             //TweenMax.to(reloadBtn, 0.5, { scale: 1, opacity: 1 });
+//             document.body.style.backgroundColor = "black";
+//         }, 1000);
+//     }
+// });
+
